56aced432a2935284b21ed00ef96d8633bea4d3e
Daniel Fishbein <128017685+LobotomyTerror@users.noreply.github.com>
2025-07-08 07:26:12 -0600

    added some starting end points for file uploads and landing page rendering

--- p2p-app/src/main.rs
+++ p2p-app/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+// #[macro_use] extern crate rocket;
+use rocket::form::{Form, FromForm};
+use rocket::{get, post, launch, routes};
+use rocket::fs::{FileServer, TempFile, relative};
+use rocket_dyn_templates::{Template, context};
+
+#[derive(FromForm, Debug)]
+struct FileUpload<'a> {
+    files: Vec<TempFile<'a>>
+}
+
+#[post("/file-upload", data = "<files>")]
+fn file_upload(files: Form<FileUpload<'_>>) -> std::io::Result<()> {
+    println!("{:?}", files);
+    // let files = files.into_inner();
+    // for (i, file) in files.iter().enumerate() {
+    //     println!("{i}: {}", file.len());
+    // }
+    Ok(())
+}
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", context! {})
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index, file_upload])
+        .mount("/assets", FileServer::from(relative!("/assets")))
+        .attach(Template::fairing())
 }
