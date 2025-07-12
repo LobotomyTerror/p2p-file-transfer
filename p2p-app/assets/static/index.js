ae9f2ec9680b543f6061379e8363b142ec06dfac
Daniel Fishbein <128017685+LobotomyTerror@users.noreply.github.com>
2025-07-12 08:21:54 -0600

    added new dependencies for application as well as fixed endpoint to save files and return a uri string with file name. frontend side is now accepting json and uri string with file name just need to add the fragment on this end for decryption purposes.

--- p2p-app/assets/static/index.js
+++ p2p-app/assets/static/index.js
@@ -1,14 +1,41 @@
 function fileSubmit() {
     const fileSubmission = document.getElementById("fileupload");
 
-    fileSubmission.addEventListener("submit", (ev) => {
+    fileSubmission.addEventListener("submit", async (ev) => {
         ev.preventDefault();
         let targetFiles = ev.target[0];
 
         const data = new FormData();
 
-        for (file of targetFiles.files)
-            data.append('files', file);
+        const iv = window.crypto.getRandomValues(new Uint8Array(12));
+        const key = await window.crypto.subtle.generateKey(
+            {
+                name: "AES-GCM",
+                length: 256,
+            },
+            true,
+            ['encrypt', 'decrypt'],
+        );
+
+        for (file of targetFiles.files) {
+            const arrayBuffer = await file.arrayBuffer();
+
+            const encryptValue = await window.crypto.subtle.encrypt(
+                {name: "AES-GCM", iv},
+                key,
+                arrayBuffer
+            );
+            // console.log(encryptValue);
+            data.append("files", new Blob([encryptValue]), file.name);
+        }
+
+        const rawKey = await window.crypto.subtle.exportKey("raw", key);
+        const key64 = btoa(String.fromCharCode(...new Uint8Array(rawKey)));
+
+        const iv64 = btoa(String.fromCharCode(...iv));
+
+        // data.append("key", key64);
+        // data.append("iv", iv64);
 
         console.log(data);
 
@@ -20,12 +47,16 @@ function fileSubmit() {
             }
         ).then(
             response => {
-                if (!response.ok) throw new Error("Failed to upload files");
-                // return response.json;
-                console.log(response);
+                if (!response.ok)
+                    throw new Error("Failed to upload files");
+                return response.json();
             }
-        )
-    })
+        ).then(
+            json => {
+                console.log(json);
+            }
+        );
+    });
 }
 
 
--- p2p-app/src/main.rs
+++ p2p-app/src/main.rs
@@ -1,23 +1,60 @@
-// #[macro_use] extern crate rocket;
+use std::fs;
+use http::{uri};
+use rocket::serde::json::Json;
+use serde::{/*Deserialize*/ Serialize};
+use uuid::Uuid;
 use rocket::form::{Form, FromForm};
 use rocket::{get, post, launch, routes};
 use rocket::fs::{FileServer, TempFile, relative};
 use rocket_dyn_templates::{Template, context};
+use nanoid::nanoid;
+
 
 #[derive(FromForm, Debug)]
 struct FileUpload<'a> {
     files: Vec<TempFile<'a>>
 }
 
-#[post("/file-upload", data = "<files>")]
-fn file_upload(files: Form<FileUpload<'_>>) -> std::io::Result<()> {
-    println!("{:?}", files);
-    // let files = files.into_inner();
-    // for (i, file) in files.iter().enumerate() {
-    //     println!("{i}: {}", file.len());
-    // }
-    Ok(())
+
+#[derive(Serialize)]
+struct UriString {
+    message: String,
+    uri_string: String
 }
+
+
+#[get("/download-file/<roomid>")]
+async fn download_file(roomid: &str) {
+    println!("{roomid}");
+}
+
+
+#[post("/file-upload", data = "<files>")]
+async fn file_upload(mut files: Form<FileUpload<'_>>) -> std::io::Result<Json<UriString>> {
+    let room_id = nanoid!(15);
+    let _ = fs::create_dir(format!("tmp/files/{room_id}"));
+
+    for (_i, file) in files.files.iter_mut().enumerate() {
+        let uuid = Uuid::new_v4();
+        let file_path = format!("tmp/files/{room_id}/{uuid}");
+        file.persist_to(&file_path).await?
+    }
+
+    let uri_builder = uri::Builder::new()
+        .scheme(uri::Scheme::HTTP)
+        .authority("127.0.0.1:8000")
+        .path_and_query(format!("/download-file/{room_id}"))
+        .build()
+        .unwrap();
+
+    let uri = UriString {
+        message: "Upload succesful".to_string(),
+        uri_string: format!("{uri_builder}"),
+    };
+
+    Ok(rocket::serde::json::Json(uri))
+}
+
 
 #[get("/")]
 fn index() -> Template {
@@ -27,7 +64,7 @@ fn index() -> Template {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, file_upload])
+        .mount("/", routes![index, file_upload, download_file])
         .mount("/assets", FileServer::from(relative!("/assets")))
         .attach(Template::fairing())
 }
